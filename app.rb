d2a6f5ac51542556267efb340090eefeadc8291d
Duncan Morrison <duncanmorrison2001@yahoo.com>
2020-11-09 17:26:30 +0000

    create spaces routes

--- app.rb
+++ app.rb
@@ -2,6 +2,8 @@ require 'sinatra/base'
 require 'sinatra/activerecord'
 require 'sinatra/flash'
 require './lib/user'
+require './lib/space'
+
 
 class MakersBnb < Sinatra::Base
   enable :sessions
@@ -12,9 +14,6 @@ class MakersBnb < Sinatra::Base
     "Hello World!"
   end
 
-<<<<<<< HEAD
-
-=======
   get '/signup' do
     erb :signup, :layout => :layout
   end
@@ -50,7 +49,19 @@ class MakersBnb < Sinatra::Base
     session[:user_id] = nil
     redirect to '/login'
   end
->>>>>>> master
 
+  get '/spaces/new' do
+    erb :index
+  end
+
+  post '/add_space' do
+    space = Space.new(name: params[:name], description: params[:description], price: params[:price], user_id: session[:user_id])
+    if space.save
+      flash[:success] = "Space created"
+      redirect to '/login'
+    else
+      redirect to '/spaces/new'
+    end
+  end
   run! if app_file == $0
 end
