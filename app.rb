6152bce2ca80ac5f06e2005ebba6d77c3a9c2578
Duncan Morrison <duncanmorrison2001@yahoo.com>
2020-11-10 13:39:01 +0000

    using some test routes for data creation, to be deleted later

--- app.rb
+++ app.rb
@@ -99,5 +99,17 @@ class MakersBnb < Sinatra::Base
       redirect to '/spaces/new'
     end
   end
+
+  # ONLY FOR TESTING UNTIL OTHER PAGES EXIST
+  get '/data_setup' do
+    user = User.create(username: 'Foo', email: "foo@example.com", password: "test")
+    user2 = User.create(username: 'Bar', email: "bar@example.com", password: "test")
+    Space.create(name: "Test Space", description: "A space for testing.", price: 10, user_id: user.id)
+  end
+
+  get '/test_reservation' do
+    reservation = Reservation.create(date: Date.new(2020, 11, 11), user_id: session[:user_id], space_id: 1, confirmed: false)
+  end
+  
   run! if app_file == $0
 end
