048eeaa611ab3fcee9d62ab03b9662cf700e8953
Duncan Morrison <duncanmorrison2001@yahoo.com>
2020-11-13 10:59:01 +0000

    blocked out past dates, improved calendar controls

--- helpers.rb
+++ helpers.rb
@@ -8,4 +8,20 @@ module ApplicationHelper
     p "I'M SENDING AN EMAIL"
     # p Pony.mail :headers => { 'Content-Type' => 'text/html' }, :from=>"noreply@makersbnb.com", :to=> @recipient.email, :subject=> subject, :body=> erb(type, :layout => nil)
   end
+
+  def next_month(date)
+    date.month < 12 ? date.month + 1 : 1
+  end
+
+  def next_year(date)
+    date.month < 12 ? date.year : date.year + 1
+  end
+
+  def last_month(date)
+    date.month == 1 ? 12 : date.month - 1
+  end
+
+  def last_year(date)
+    date.month == 1 ? date.year - 1 : date.year
+  end
 end
